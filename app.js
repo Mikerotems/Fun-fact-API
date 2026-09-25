16f1e162a0e8d1f3da1bbbc351dfe6bb8a931ef6
mike_rotems <mymaster17@gmail.com>
2026-09-25 10:07:42 +0100

    port updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2701;
 const publicDir = path.join(__dirname, 'public');
 
 // Serve static frontend files
